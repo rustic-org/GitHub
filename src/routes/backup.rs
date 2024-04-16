07e81b464b7682a0e0805547490d37989f6235ca
rustic-monkey <vignesh.sivanandarao@gmail.com>
2024-04-16 18:55:21 -0500

    Download JSON unparseable files Send branch and download location via payload Remove unused dependencies

--- src/routes/backup.rs
+++ src/routes/backup.rs
@@ -1,9 +1,8 @@
 use std::{fs, path, sync, collections};
-use std::io::Write;
+use std::io::{Write, copy, Error};
 
 use actix_web::{HttpRequest, HttpResponse, web};
 use actix_web::http::StatusCode;
-
 
 use serde::{Deserialize, Serialize};
 
@@ -23,41 +22,55 @@ pub fn verify_token(request: &HttpRequest,
     if let Some(authorization) = headers.get("authorization") {
         let auth = authorization.to_str().unwrap().to_string();
         if format!("Bearer {}", config.authorization) == auth {
-            return if let Some(path) = headers.get("content-location") {
-                if let Ok(location_str) = path.to_str() {
-                    AuthResponse { ok: true, repository: location_str.to_string() }
+            let mut location = String::new();
+            if let Some(header_value) = headers.get("content-location") {
+                if let Ok(location_str) = header_value.to_str() {
+                    location = location_str.to_string();
                 } else {
-                    AuthResponse { ok: false, repository: "".to_string() }
+                    log::error!("Failed to convert 'content-location' header to string");
                 }
-            } else {
-                AuthResponse { ok: false, repository: "".to_string() }
+            }
+            let (repository, branch) = {
+                let mut parts = location.split(';');
+                let repository = parts.next().unwrap_or("");
+                let branch = parts.next().unwrap_or("");
+                (repository.to_string(), branch.to_string())
             };
+            return AuthResponse { ok: true, repository, branch };
         } else {
             log::error!("Invalid token: {}", auth);
-            AuthResponse { ok: false, repository: String::new() }
+            AuthResponse { ok: false, repository: String::new(), branch: String::new() }
         }
     } else {
         log::error!("No auth header received");
-        AuthResponse { ok: false, repository: String::new() }
+        AuthResponse { ok: false, repository: String::new(), branch: String::new() }
     }
 }
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Payload {
     #[serde(default = "default_hash")]
+    // sample: {'src/plain/.keep': 'some text'}
     create: collections::HashMap<String, String>,
-    // sample: {'src/plain/.keep': ''}
+
     #[serde(default = "default_hash")]
+    // sample: {'src/plain/main.py': 'src/main.py'} - move/rename
     modify: collections::HashMap<String, String>,
-    // sample: {'src/plain/main.py': 'src/main.py'}
+
     #[serde(default = "default_vec")]
-    remove: Vec<String>,  // sample: ['matrix/executor.py', 'src/plain/main.py']
+    // sample: ['matrix/executor.py', 'src/plain/main.py']
+    remove: Vec<String>,
+
+    #[serde(default = "default_vec")]
+    // sample: ['src/sample.png'] - since bytes can't be JSON encoded
+    download: Vec<String>,
 }
 
 /// Struct for the authentication response.
 pub struct AuthResponse {
     pub ok: bool,
     pub repository: String,
+    pub branch: String
 }
 
 fn default_vec() -> Vec<String> { Vec::new() }
@@ -110,6 +123,52 @@ pub fn delete_file(destination: &path::PathBuf, source: &path::Path) -> (u16, St
     (404, error)
 }
 
+pub fn fallback_clone(github_source: &path::PathBuf,
+                      repository: &String,
+                      default_response: HttpResponse) -> HttpResponse {
+    let dest = github_source.join(repository);
+    if let Err(err) = fs::remove_dir_all(&dest) {
+        log::error!("Error deleting out of sync repo: {:?}", err);
+        return default_response;
+    } else {
+        log::info!("Deleted out of sync repo: {:?}", &dest);
+    }
+    let repo_validation = routes::intro::validate_repo(
+        &repository, &github_source,
+    );
+    if repo_validation.ok && repo_validation.cloned {
+        return HttpResponse::Ok().finish();
+    }
+    default_response
+}
+
+pub async fn download_file(auth_response: &AuthResponse,
+                     config: &squire::settings::Config,
+                     downloadable: &String) -> Result<(), Error> {
+    let destination = &config.github_source
+        .join(&auth_response.repository)
+        .join(downloadable);
+    let url = format!("https://raw.githubusercontent.com/{}/{}/{}",
+                      auth_response.repository, auth_response.branch, downloadable);
+    let response = match reqwest::get(url).await {
+        Ok(res) => res,
+        Err(err) => return Err(Error::new(std::io::ErrorKind::Other, err)),
+    };
+    let response = match response.error_for_status() {
+        Ok(res) => res,
+        Err(err) => return Err(Error::new(std::io::ErrorKind::Other, err)),
+    };
+    let mut dest_file = match fs::File::create(destination) {
+        Ok(file) => file,
+        Err(err) => return Err(err),
+    };
+    let bytes = response.bytes().await.map_err(|e| Error::new(std::io::ErrorKind::Other, e))?;
+    match copy(&mut bytes.as_ref(), &mut dest_file) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err.into()),
+    }
+}
+
 #[post("/backup")]
 pub async fn backup_endpoint(request: HttpRequest,
                              payload: web::Json<Payload>,
@@ -134,7 +193,7 @@ pub async fn backup_endpoint(request: HttpRequest,
         log::info!("Repository '{}' was cloned, so no point in proceeding further", &auth_response.repository);
         return HttpResponse::Ok().finish();
     }
-    // todo: instead of throwing errors, simply delete and re-clone
+
     for (filepath, content) in &payload.create {
         let true_path = &config.github_source
             .join(&auth_response.repository)
@@ -145,7 +204,9 @@ pub async fn backup_endpoint(request: HttpRequest,
             if let Err(err) = fs::create_dir_all(parent) {
                 let error = format!("Error creating directories: {}", err);
                 log::error!("{}", error);
-                return HttpResponse::ExpectationFailed().json(error);
+                return fallback_clone(&config.github_source,
+                                      &auth_response.repository,
+                                      HttpResponse::ExpectationFailed().json(error));
             }
         }
 
@@ -154,7 +215,9 @@ pub async fn backup_endpoint(request: HttpRequest,
             Err(err) => {
                 let error = format!("Error creating file: {}", err);
                 log::error!("{}", error);
-                return HttpResponse::ExpectationFailed().json(error);
+                return fallback_clone(&config.github_source,
+                                      &auth_response.repository,
+                                      HttpResponse::ExpectationFailed().json(error));
             }
         };
         match file.write_all(content.as_bytes()) {
@@ -162,7 +225,9 @@ pub async fn backup_endpoint(request: HttpRequest,
             Err(err) => {
                 let error = format!("Error writing to file: {}", err);
                 log::error!("{}", error);
-                return HttpResponse::ExpectationFailed().json(error);
+                return fallback_clone(&config.github_source,
+                                      &auth_response.repository,
+                                      HttpResponse::ExpectationFailed().json(error));
             }
         }
     }
@@ -178,7 +243,9 @@ pub async fn backup_endpoint(request: HttpRequest,
             Err(err) => {
                 let error = format!("Failed to move file [{:?}] to [{:?}] - {}", src, dst, err);
                 log::error!("{}", error);
-                return HttpResponse::ExpectationFailed().json(error);
+                return fallback_clone(&config.github_source,
+                                      &auth_response.repository,
+                                      HttpResponse::ExpectationFailed().json(error));
             }
         }
     }
@@ -188,7 +255,21 @@ pub async fn backup_endpoint(request: HttpRequest,
             .join(removable);
         let (code, out) = delete_file(destination, &config.github_source);
         if code != 200 {
-            return HttpResponse::build(StatusCode::from_u16(code).unwrap()).json(out);
+            return fallback_clone(&config.github_source,
+                                  &auth_response.repository,
+                                  HttpResponse::build(StatusCode::from_u16(code).unwrap()).json(out));
+        }
+    }
+    for downloadable in &payload.download {
+        match download_file(&auth_response, &config, downloadable).await {
+            Ok(_) => log::info!("{} downloaded successfully!", downloadable),
+            Err(err) => {
+                let error = format!("Error downloading file: {}", err);
+                log::error!("{}", error);
+                return fallback_clone(&config.github_source,
+                                      &auth_response.repository,
+                                      HttpResponse::ExpectationFailed().json(error));
+            },
         }
     }
     HttpResponse::Ok().finish()
